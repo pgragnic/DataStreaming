b98ed403f90f827c6fc524b125bff303a67b4808
pgragnic <pgragnic@hotmail.com>
2021-10-26 11:44:12 +0200

    update directory structure

--- 2. Data Ingestion with Kafka & Kafka Streaming/Project - Optimizing public transportation/producers/models/producer.py
+++ 2. Data Ingestion with Kafka & Kafka Streaming/Project - Optimizing public transportation/producers/models/producer.py
@@ -38,9 +38,9 @@ class Producer:
         #
         #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            "BROKER_URL": "PLAINTEXT://localhost:9092",
+            "SCHEMA_REGISTRY_URL": "http://localhost:8081",
+            "group.id": f"{self.topic_name}",
         }
 
         # If the topic does not already exist, try to create it
@@ -49,30 +49,29 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+            {"bootstrap.servers": self.broker_properties["BROKER_URL"]}, 
+            schema_registry=CachedSchemaRegistryClient(
+                {"url": self.broker_properties["SCHEMA_REGISTRY_URL"]},
+            )
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        client = AdminClient({"bootstrap.servers": self.broker_properties["BROKER_URL"]})
+        topic = NewTopic(self.topic_name, 
+            num_partitions=self.num_partitions, 
+            replication_factor=self.num_replicas,
+        )
+
+        client.create_topics([topic])
 
     def time_millis(self):
         return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        self.producer.flush()
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
